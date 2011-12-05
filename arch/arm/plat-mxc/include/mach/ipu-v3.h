fed4146772f14e9c84330c3f7e364d3f450c9db2
Jason Chen <jason.chen@linaro.org>
2011-12-05 18:18:59 +0800

    imx6q-sabrelite: add ipuv3 fb dt support
    Signed-off-by: Jason Chen <jason.chen@linaro.org>

--- arch/arm/plat-mxc/include/mach/ipu-v3.h
+++ arch/arm/plat-mxc/include/mach/ipu-v3.h
@@ -691,7 +691,7 @@ uint32_t bytes_per_pixel(uint32_t fmt);
 struct ipuv3_fb_platform_data {
 	char				disp_dev[32];
 	u32				interface_pix_fmt;
-	char				*mode_str;
+	char				mode_str[32];
 	int				default_bpp;
 	bool				int_clk;
 
